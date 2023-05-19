08edd680e3db534c127dfbdfc3b386e20b014723
Stuart Turner <stuart@turner-isageek.com>
2023-05-19 10:50:11 -0500

    Add Market Tests

---
+++ Regular.Polygon.Tests/MarketTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Regular.Polygon.Tests;
+
+public class MarketTests : IClassFixture<PolygonFixture>
+{
+	private readonly PolygonFixture _fixture;
+
+	public MarketTests(PolygonFixture fixture)
+	{
+		_fixture = fixture;
+	}
+
+	[Fact]
+	public Task GetMarketStatus() =>
+		// all we're looking for is successful api query
+		_fixture.Client.GetMarketStatus();
+
+	[Fact]
+	public Task GetMarketHolidays() =>
+		// all we're looking for is successful api query
+		_fixture.Client.GetMarketHolidays();
+}
---
+++ Regular.Polygon.Tests/PolygonFixture.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Regular.Polygon.Tests;
+
+public class PolygonFixture
+{
+	private readonly ServiceProvider _serviceProvider;
+
+	public IPolygonApi Client { get; }
+
+	public PolygonFixture()
+	{
+		var configuration = new ConfigurationBuilder()
+			.AddJsonFile("secrets.json", optional: true)
+			.AddEnvironmentVariables()
+			.Build();
+
+		var services = new ServiceCollection();
+
+		var apiKey = configuration["ApiKey"];
+		Guard.IsNotNullOrWhiteSpace(apiKey);
+		services.AddPolygonApi(apiKey);
+
+		_serviceProvider = services.BuildServiceProvider();
+		Client = _serviceProvider.GetRequiredService<IPolygonApi>();
+	}
+}
