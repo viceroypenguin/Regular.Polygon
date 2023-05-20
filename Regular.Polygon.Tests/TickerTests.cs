a5f6e60b993e2ac9dd195ddffbf6e9fdab9637c2
Stuart Turner <stuart@turner-isageek.com>
2023-05-19 19:11:58 -0500

    Improve coverage

--- Regular.Polygon.Tests/TickerTests.cs
+++ Regular.Polygon.Tests/TickerTests.cs
@@ -41,8 +41,8 @@ public class TickerTests : IClassFixture<PolygonFixture>
 	public async Task SearchTickers()
 	{
 		// all we're looking for is successful api query
-		var tickers = await _fixture.Client.SearchTickers(new() { Ticker = "MSFT", });
-		Guard.IsGreaterThan(tickers.Count ?? 0, 0);
+		var tickers = await _fixture.Client.SearchTickersAll(new() { Ticker = "MSFT", });
+		Guard.IsGreaterThan(tickers.Count, 0);
 	}
 
 	[Fact]
