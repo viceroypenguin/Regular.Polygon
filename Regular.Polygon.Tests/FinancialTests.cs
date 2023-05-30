f2603951cf36b7c9be27a20a6be4359424ebfd5c
Stuart Turner <stuart@turner-isageek.com>
2023-05-30 08:27:21 -0500

    Add unt test

---
+++ Regular.Polygon.Tests/FinancialTests.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Diagnostics;
+using Regular.Polygon.Ticker;
+using Xunit;
+
+namespace Regular.Polygon.Tests;
+
+public class FinancialTests : IClassFixture<PolygonFixture>
+{
+	private readonly PolygonFixture _fixture;
+
+	public FinancialTests(PolygonFixture fixture)
+	{
+		_fixture = fixture;
+	}
+
+	[Fact]
+	public Task GetFinancials() =>
+		// all we're looking for is successful api query
+		_fixture.Client.GetStockFinancials(new() { Ticker = "AAPL", });
+}
