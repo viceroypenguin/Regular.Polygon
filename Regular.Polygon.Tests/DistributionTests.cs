b712132e23b037ccf00457db941a4ffd407ab10f
Stuart Turner <stuart@turner-isageek.com>
2023-05-30 19:38:14 -0500

    Add Stock Splits API

---
+++ Regular.Polygon.Tests/DistributionTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Regular.Polygon.Tests;
+
+public class DistributionTests : IClassFixture<PolygonFixture>
+{
+	private readonly PolygonFixture _fixture;
+
+	public DistributionTests(PolygonFixture fixture)
+	{
+		_fixture = fixture;
+	}
+
+	[Fact]
+	public Task GetStockSplit() =>
+		// all we're looking for is successful api query
+		_fixture.Client.GetStockSplits(new() { Ticker = "AAPL", });
+
+}
---
+++ Regular.Polygon/Distribution/IPolygonApi.Distribution.cs
@@ -0,0 +1,15 @@
+﻿using Regular.Polygon.Distribution;
+
+namespace Regular.Polygon;
+
+public partial interface IPolygonApi
+{
+	/// <summary>
+	/// Get a list of historical stock splits, including the ticker symbol, the execution date, and the factors of the split ratio.
+	/// </summary>
+	/// <param name="request">Request object to hold parameters for the Stock Split api.</param>
+	/// <param name="cancellationToken">Cancellation token that can be used to cancel the operation.</param>
+	/// <returns>The current trading status of the exchanges and overall financial markets</returns>
+	[Get("/v3/reference/splits")]
+	Task<PolygonResponse<IReadOnlyList<StockSplit>>> GetStockSplits([Query] StockSplitRequest? request = null, CancellationToken cancellationToken = default);
+}
