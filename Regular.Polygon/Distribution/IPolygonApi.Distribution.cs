12af4f2d7887a74e9a07b48a3d1cb2621562d0ce
Stuart Turner <stuart@turner-isageek.com>
2023-05-30 20:07:56 -0500

    Add Distributions APIs (#15)
    * Add Stock Splits API
    * Add Dividend API

---
+++ Regular.Polygon/Distribution/IPolygonApi.Distribution.cs
@@ -0,0 +1,24 @@
+﻿using Regular.Polygon.Distribution;
+
+namespace Regular.Polygon;
+
+public partial interface IPolygonApi
+{
+	/// <summary>
+	/// Get a list of historical stock splits, including the ticker symbol, the execution date, and the factors of the split ratio.
+	/// </summary>
+	/// <param name="request">Request object to hold parameters for the Stock Split api.</param>
+	/// <param name="cancellationToken">Cancellation token that can be used to cancel the operation.</param>
+	/// <returns>The current trading status of the exchanges and overall financial markets</returns>
+	[Get("/v3/reference/splits")]
+	Task<PolygonResponse<IReadOnlyList<StockSplit>>> GetStockSplits([Query] StockSplitRequest? request = null, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Get a list of historical cash dividends, including the ticker symbol, declaration date, ex-dividend date, record date, pay date, frequency, and amount.
+	/// </summary>
+	/// <param name="request">Request object to hold parameters for the Dividend api.</param>
+	/// <param name="cancellationToken">Cancellation token that can be used to cancel the operation.</param>
+	/// <returns>The current trading status of the exchanges and overall financial markets</returns>
+	[Get("/v3/reference/dividends")]
+	Task<PolygonResponse<IReadOnlyList<Dividend>>> GetDividends([Query] DividendRequest? request = null, CancellationToken cancellationToken = default);
+}
---
+++ Regular.Polygon/Distribution/StockSplit.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Regular.Polygon.Distribution;
+
+/// <summary>
+/// Request class to hold parameters for the Stock Split api
+/// </summary>
+public class StockSplitRequest
+{
+	/// <summary>
+	/// Specify a ticker symbol. 
+	/// </summary>
+	[AliasAs("ticker")]
+	public string? Ticker { get; set; }
+
+	/// <summary>
+	/// Search ticker symbol string comparison greater than.
+	/// </summary>
+	[AliasAs("ticker.gte")]
+	public string? TickerGreaterThan { get; set; }
+
+	/// <summary>
+	/// Search ticker symbol string comparison less than.
+	/// </summary>
+	[AliasAs("ticker.lte")]
+	public string? TickerLessThan { get; set; }
+
+	/// <summary>
+	/// Query for splits on the execution date
+	/// </summary>
+	[AliasAs("execution_date")]
+	public DateOnly? ExecutionDate { get; set; }
+
+	/// <summary>
+	/// Query for splits before the execution date
+	/// </summary>
+	[AliasAs("execution_date.lte")]
+	public DateOnly? ExecutionDateBefore { get; set; }
+
+	/// <summary>
+	/// Query for splits after the execution date
+	/// </summary>
+	[AliasAs("execution_date.gte")]
+	public DateOnly? ExecutionDateAfter { get; set; }
+
+	/// <summary>
+	/// Query for reverse stock splits. A split ratio where <see cref="StockSplit.SplitFrom"/> is greater than <see
+	/// cref="StockSplit.SplitTo"/> represents a reverse split. By default this filter is not used.
+	/// </summary>
+	[AliasAs("reverse_split")]
+	public bool? ReverseSplit { get; set; }
+
+	/// <summary>
+	/// Sort field used for ordering.
+	/// </summary>
+	[AliasAs("sort")]
+	public string? Sort { get; set; }
+
+	/// <summary>
+	/// Order results based on the <see cref="Sort"/> field.
+	/// </summary>
+	[AliasAs("order")]
+	public SortOrder? Order { get; set; }
+
+	/// <summary>
+	/// Limit the number of results returned, default is 100 and max is 1000.
+	/// </summary>
+	[AliasAs("limit")]
+	public int? Limit { get; set; }
+}
+
+/// <summary>
+/// Information about a stock split
+/// </summary>
+/// <param name="Ticker">The ticker symbol of the stock split.</param>
+/// <param name="ExecutionDate">The execution date of the stock split. On this date the stock split was applied.</param>
+/// <param name="SplitFrom">The second number in the split ratio. For example: In a 2-for-1 split, <paramref
+/// name="SplitFrom"/> would be 1.</param>
+/// <param name="SplitTo">The first number in the split ratio. For example: In a 2-for-1 split, <paramref
+/// name="SplitTo"/> would be 2.</param>
+public record StockSplit(
+	string Ticker,
+	[property: JsonPropertyName("execution_date")]
+	DateOnly ExecutionDate,
+	[property: JsonPropertyName("split_from")]
+	decimal SplitFrom,
+	[property: JsonPropertyName("split_to")]
+	decimal SplitTo);
