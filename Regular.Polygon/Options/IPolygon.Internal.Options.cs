00544907860ad78542891cfd0c3db1e5e3f7333e
Stuart Turner <stuart@turner-isageek.com>
2024-06-03 09:32:20 -0500

    Update API endpoint

--- Regular.Polygon/Options/IPolygon.Internal.Options.cs
+++ Regular.Polygon/Options/IPolygon.Internal.Options.cs
@@ -6,7 +6,7 @@ namespace Regular.Polygon;
 internal partial interface IPolygonApiRefit
 {
 	[Get("/v3/reference/options/contracts/{ticker}")]
-	Task<PolygonResponse<OptionsContractDetail>> GetOptionsContractDetail(string ticker, [Query] DateOnly? date = null, CancellationToken cancellationToken = default);
+	Task<PolygonResponse<OptionsContractDetail>> GetOptionsContractDetail(string ticker, [Query, AliasAs("as_of")] DateOnly? date = null, CancellationToken cancellationToken = default);
 
 	[Get("/v3/reference/options/contracts")]
 	Task<PolygonResponse<IReadOnlyList<OptionsContractDetail>>> SearchOptionsContracts([Query] OptionsContractSearch? request = null, CancellationToken cancellationToken = default);
